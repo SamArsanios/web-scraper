a62de6b129621cf6f29fd28b8ebe529f76c1cc23
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-26 19:51:06 +0300

    Rename method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 require 'HTTParty'
 require 'byebug'
-def wld_pop
+
+def world_population
   unparsed_page = HTTParty.get('https://www.worldometers.info/world-population/')
   parsed_page = Nokogiri::HTML(unparsed_page)
   population_stats = []
+  
   nations = parsed_page.css('table#popbycountry > tbody > tr').map(&:text).count
   i = 1
   while i < nations
