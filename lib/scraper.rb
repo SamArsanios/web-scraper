9f5647d2c657d85813e4ad5fbbc9911b8e9b7d51
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-26 19:52:30 +0300

    Fix linter errors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ def world_population
   unparsed_page = HTTParty.get('https://www.worldometers.info/world-population/')
   parsed_page = Nokogiri::HTML(unparsed_page)
   population_stats = []
-  
+
   nations = parsed_page.css('table#popbycountry > tbody > tr').map(&:text).count
   i = 1
   while i < nations
