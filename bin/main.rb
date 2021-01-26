23a759f2a726d8a74f619c00a776ed9e2c0df5e5
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-26 16:42:17 +0300

    add instantiator

---
+++ bin/main.rb
@@ -0,0 +1,12 @@
+# require_relative '../lib/scraper.rb'
+
+# scraper = Scraper.new
+# records = scraper.world_population
+# all_nations = scraper.all_nations
+
+# (0...all_nations).each do |index|
+#   puts "-------------------- index: #{index + 1} --------------------"
+#   puts "Records: #{records[index]}"
+ 
+#   # byebug
+# end
