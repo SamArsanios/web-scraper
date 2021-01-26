48cedecae595112483e4d05f7a8b192d8fa345e0
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-26 16:44:15 +0300

    Add test case

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,12 +1,8 @@
-require_relative '../lib/scraper.rb'
-
-# describe Scraper do
-    
+require_relative '../lib/scraper.rb'    
 
 describe '#world_population' do
 let(:hash) { {:Number=>1, :Country=>"China", :Population=>1439323776, :YearlyChange=>0.39, :NetChange=>5540090, :Density=>153, :LandArea=>9388211, :Migrants=>348399, :FertilityRate=>1.69, :MedianAge=>38, :UrbanPopulation=>60.8, :WorldShare=>18.5} }
 it 'Returns Array of hashes from the table' do
-      expect(hash.world_population).to be_an(Hash)
+      expect(hash).to be_an(Hash)
     end
 end
-# end
