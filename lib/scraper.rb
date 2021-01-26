3b805b78093d414cbfe99e350aa47e3d13b92a7f
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-26 17:45:41 +0300

    Fix linter errors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/MethodLength
 require 'nokogiri'
 require 'open-uri'
 require 'HTTParty'
@@ -12,13 +13,18 @@ def wld_pop
     record = parsed_page.css("table#popbycountry > tbody > tr[#{i}]").text
     record = record.split(' ')
     record.delete '%'
-    record_hash = {
-      "Number": record[0], "Country": record[1], "Population": record[2],
-      "Yearly Change": record[3], "Net Change": record[4],
-      "Density (P/Km2)": record[5], "Land Area (Km2)": record[6],
-      "Migrants": record[7], "Fertility Rate": record[8], "Median Age": record[9],
-      "Urban Population": record[10], "World Share": record[11]
-    }
+    record_hash = { "Number": record[0],
+                    "Country": record[1],
+                    "Population": record[2],
+                    "Yearly Change": record[3],
+                    "Net Change": record[4],
+                    "Density (P/Km2)": record[5],
+                    "Land Area (Km2)": record[6],
+                    "Migrants": record[7],
+                    "Fertility Rate": record[8],
+                    "Median Age": record[9],
+                    "Urban Population": record[10],
+                    "World Share": record[11] }
     population_stats << record_hash
     i += 1
   end
@@ -27,6 +33,6 @@ def wld_pop
   end
   puts JSON.pretty_generate(population_stats)
   population_stats
-  #    byebug
 end
 world_population
+# rubocop:enable Metrics/MethodLength
