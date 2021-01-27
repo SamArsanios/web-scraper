9ec0690f60a9cb879a92f5bd75daa8d17450d863
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-27 18:54:43 +0300

    Fix linter errors

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,23 +1,23 @@
 require_relative '../lib/scraper'
 
 describe Scraper do
-  let(:scraper) {Scraper.new("https://www.worldometers.info/world-population/")}
+  let(:scraper) { Scraper.new('https://www.worldometers.info/world-population/') }
 
-    describe '#world_population' do
-      it 'Returns Array of hashes from the table' do
+  describe '#world_population' do
+    it 'Returns Array of hashes from the table' do
       expect(scraper.world_population).to be_an(Array)
-      end
     end
+  end
 
-    describe '#all_nations' do
-      it 'Returns an integer' do
-        expect(Integer).to be_truthy 
-      end
+  describe '#all_nations' do
+    it 'Returns an integer' do
+      expect(Integer).to be_truthy
     end
+  end
 
-    describe '#find_country_by_name' do
+  describe '#find_country_by_name' do
     it 'Returns a Hash' do
-      expect(scraper.find_country_by_name('Turkey')).to be_an(Array) 
+      expect(scraper.find_country_by_name('Turkey')).to be_an(Array)
     end
   end
-  end
+end
